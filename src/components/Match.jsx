0907d09c987321b4c234c0338bd1f14b49e176a0
Maksym Pelyno <fteq.qwwerty.com@gmail.com>
2024-04-26 18:46:30 +0300

    Save feature done

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useState, useEffect } from "react";
 import { LeagueContext } from "../context/LeagueContext";
-import { getMatchData } from "../services/MatchService";
+import { getMatchData, deleteMatch, createAndDownloadFile } from "../services/MatchService";
 import { Link } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 
 import MatchModal from "./MatchModal";
 import "../styles/Match.css";
@@ -12,9 +12,27 @@ const Match = () => {
   const [matches, setMatches] = useState([]);
   const [selectedMatch, setSelectedMatch] = useState(null);
 
+  const username = sessionStorage.getItem("username");
+
   const handleTeamSelect = (team) => {
-    console.log("hui " + team);
     setSelectedTeam(team);
+  };
+
+  const handleDelete = (matchId) => {
+    deleteMatch(matchId)
+      .then((data) => {
+        const updatedMatches = matches.filter((match) => match.match_id !== matchId);
+        setMatches(updatedMatches);
+        setSelectedMatch(null);
+        toast.success("Match deleted successfully.", {
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      })
+      .catch((error) => {
+        toast.error("Cannot delete, try again.", {
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      });
   };
 
   useEffect(() => {
@@ -33,12 +51,42 @@ const Match = () => {
       });
   };
 
+  const handleDownloadPDF = async () => {
+    try {
+      await createAndDownloadFile("pdf");
+      toast.success("Products downloaded successfully");
+    } catch (error) {
+      console.error("Error downloading PDF:", error);
+      toast.error(error.response?.data?.message || "Error downloading PDF");
+    }
+  };
+  const handleDownloadJSON = async () => {
+    try {
+      await createAndDownloadFile("json");
+      toast.success("Products downloaded successfully");
+    } catch (error) {
+      console.error("Error downloading PDF:", error);
+      toast.error(error.response?.data?.message || "Error downloading PDF");
+    }
+  };
+
   return (
     <div className="match-container">
       <ToastContainer />
 
-      <h2 className="season-heading">Season: {selectedSeason ? selectedSeason.name : "No season selected"}</h2>
       <div className="matches-container">
+        <h2 className="season-heading">Season: {selectedSeason ? selectedSeason.name : "No season selected"}</h2>
+        {username === "admin" && (
+          <div className="button-container">
+            <button className="download-button" onClick={() => handleDownloadPDF()}>
+              Download PDF
+            </button>
+            <button className="download-button" onClick={() => handleDownloadJSON()}>
+              Download JSON
+            </button>
+          </div>
+        )}
+
         {matches.map((match) => (
           <div key={match.match_id} className="match-item" onClick={() => setSelectedMatch(match)}>
             <div className="team">
@@ -59,6 +107,11 @@ const Match = () => {
                 {match.awayTeam.name}{" "}
               </Link>
             </div>
+            {username === "admin" && (
+              <button className="delete-button" onClick={() => handleDelete(match.match_id)}>
+                Delete
+              </button>
+            )}
           </div>
         ))}
       </div>
--- src/services/MatchService.js
+++ src/services/MatchService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { saveAs } from "file-saver";
 
 const API_URL = "http://localhost:3001/match";
 
@@ -30,6 +31,49 @@ export const createMatch = async ({ season_id, hometeam_id, awayteam_id, stadium
     await axios.post(API_URL, data);
     return true;
   } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteMatch = async (matchId) => {
+  try {
+    const role = sessionStorage.getItem("username");
+    const response = await axios.delete(`${API_URL}/${matchId}`, {
+      headers: {
+        Role: role.toLowerCase(),
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting match:", error);
+    throw new Error("Something went wrong while deleting match");
+  }
+};
+
+export const createAndDownloadFile = async (fileType) => {
+  try {
+    let endpoint = `${API_URL}/create-pdf`;
+    let fileName = "matches.pdf";
+    let responseType = "application/pdf";
+
+    if (fileType === "json") {
+      endpoint = `${API_URL}/create-json`;
+      fileName = "matches.json";
+      responseType = "application/json";
+    }
+
+    await axios.post(endpoint);
+    const res = await axios.get(`${API_URL}/fetch-${fileType}`, {
+      responseType: "blob",
+    });
+    const fileBlob = new Blob([res.data], {
+      type: `aplication/${responseType}`,
+    });
+
+    saveAs(fileBlob, fileName);
+    return true;
+  } catch (error) {
+    console.error("Error creating or downloading file:", error);
     throw error;
   }
 };
